1ab72ce20a93c5421e602665a4484de2b25946d6
RitaPintoBissApp <rpinto@blissapplications.com>
2023-09-19 17:41:19 +0100

    final commit

--- app/src/main/java/com/example/recyclerview/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapter/ItemAdapter.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.model.Affirmation
 import com.example.recyclerview.R
-import com.example.recyclerview.model.Affirmation
 
 /**
  * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
@@ -24,6 +25,7 @@ class ItemAdapter(
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
@@ -51,5 +53,6 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 }
